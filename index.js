e9e229ba32f8c9a0086b5f367f51d3ced4093419
Shawn Rupert <truper63@gmail.com>
2022-06-28 12:49:44 -0400

    Created Templates

--- index.js
+++ index.js
@@ -5,10 +5,34 @@ const { dirname } = require('path');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const productData = JSON.parse(data);
 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+        const cardHtml = productData.map(element => replaceTemplate(tempCard, element));
+
+        var productCards = '';
+        var card = '';
+        productData.forEach(element => {
+            card = tempCard;
+            card.replace('{%ID%}', element.id);
+            card.replace('{%IMAGE%}', element.image);
+            card.replace('{%PRODUCTNAME%}', element.productName);
+            element.organic ? card.replace('{%NOT_ORGANIC%}', 'card__detail--organic') 
+                            : card.replace('{%NOT_ORGANIC%}', '');
+            card.replace('{%QUANTITY%}', element.quantity);
+            card.replace('{%PRICE%}', element.price);
+            productCards += card;
+        });
+        const overview = tempOverview.replace('{%PRODUCT_CARDS%}', productCards);
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(overview);
+    } else if (pathName === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(productPage);
     } else if (pathName === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
